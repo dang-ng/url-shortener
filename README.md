5cea26892df5507d9decb9bff332796a7e7377ea
derek089 <nguyen.hai.dang@icloud.com>
2022-09-11 20:28:15 +0700

    1st commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# URL Shortener and File sharing
+
+#### Video Demo: https://youtu.be/n9oz_E9DIsM
+
+#### Description:
+
+There maybe many URL shorteners projects out there, but i found it quite great to start with because the project require to include many technologies learned in the lecture like python, flask, bootstrap, storing database and user cookies ... which is really help me to dive in and understand more about apply these technologies to real life.
+
+This app includes 2 parts: URL shortener and file upload into short URL
+Both are great for sharing with friends
+First you only need to input your desired URL you want to save for sharing/later uses
+Then you input new easy-remember URL of your own
+Click Shorten and voilà, new URL has been created
+Later when needed, just copy new URL and send it to your friends or public elsewhere
+when they go to your link, it will redirect to original URL
+All your new URLs are saved under cookies
+Click it and it will also redirect to original URL
+The second part is file sharing, which also under the form of new URL
+Click Choose File to select any file that you want to share
+Name of your new URL
+And there you go: the new URL generated for sharing too!
+Just share it to your friends and they can see your file, or new photos!
+Whenever your input URLs that not exists yet, it will redirect to 404 page
+Go back to home page and try again :)
+That's it! thank you for watching! :)
---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
+import json
+import os.path
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = 'abc'
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', cookies = session.keys())
+
+
+@app.route('/url-cua-toi', methods=['GET', 'POST'])
+def yoururl():
+    if request.method == 'POST':
+        urls = {}
+        if os.path.exists('urls.json'):
+            with open('urls.json') as filecheck:
+                urls = json.load(filecheck)
+        if request.form['url-ra'] in urls.keys():
+            flash('Short name already exists')
+            return redirect(url_for('home'))
+
+        if 'url-vao' in request.form.keys():
+            urls[request.form['url-ra']] = {'url': request.form['url-vao']}
+        else:
+            f = request.files['file']
+            full_name = request.form['url-ra'] + secure_filename(f.filename)
+            f.save(
+                'C:/Users/haida/Downloads/My Projects/url-shortener/static/user_files/' + full_name)
+            urls[request.form['url-ra']] = {'file': full_name}
+
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+            session[request.form['url-ra']] = True
+        return render_template('your_url.html', ketqua=request.form['url-ra'])
+    else:
+        return redirect(url_for('home'))
+
+
+@app.route('/<string:ma>')
+def return_url(ma):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as url_file:
+            urls = json.load(url_file)
+            if ma in urls.keys():
+                if 'url' in urls[ma].keys():
+                    return redirect(urls[ma]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[ma]['file']))
+    return abort(404)
+
+
+@app.errorhandler(404)
+def page_khong_tim_thay(error):
+    return render_template('page_not_found.html'), 404
+
+@app.route('/api')
+def api():
+    return jsonify(list(session.keys()))
